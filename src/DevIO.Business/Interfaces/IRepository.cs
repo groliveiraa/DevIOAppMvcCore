e92511e68caa1309e4805710cff80eeceb75de7e
Gustavo Rodrigues <gustavo.oliveira8258@gmail.com>
2020-06-27 16:50:21 -0300

    Injeção de Dependências: Interfaces e Repository. Configuração do Startup para injeção de dependências.

---
+++ src/DevIO.Business/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using DevIO.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        // Repositório Genérico - Injeção de Dependência
+        Task Adicionar(TEntity entity);
+        Task<TEntity> ObterPorId(Guid id);
+        Task<List<TEntity>> ObterTodos();
+        Task Atualizar(TEntity entity);
+        Task Remover(Guid id);
+        Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate);
+        Task<int> SaveChanges();
+    }
+}
