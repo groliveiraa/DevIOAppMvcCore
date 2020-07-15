b162977cb1f74214a5af3601298e1523fdba122d
Gustavo Rodrigues <gustavo.oliveira8258@gmail.com>
2020-07-14 21:30:52 -0300

    Ajustes e Customização das Views

--- src/DevIO.App/ViewModels/EnderecoViewModel.cs
+++ src/DevIO.App/ViewModels/EnderecoViewModel.cs
@@ -18,6 +18,7 @@ namespace DevIO.App.ViewModels
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(50, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 1)]
         public string Numero { get; set; }
+
         public string Complemento { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
