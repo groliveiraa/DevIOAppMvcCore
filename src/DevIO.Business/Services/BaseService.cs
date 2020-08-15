987ab96eb83086439894741af9627d20e916f819
Gustavo Rodrigues <gustavo.oliveira8258@gmail.com>
2020-08-15 16:27:34 -0300

    Validando as entidades de negócio - Utilizando o FluentValidation

---
+++ src/DevIO.Business/Services/BaseService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using FluentValidation.Results;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+using System.Text;
+using FluentValidation;
+using DevIO.Business.Models;
+
+namespace DevIO.Business.Services
+{
+    public abstract class BaseService
+    {
+        protected void Notificar(ValidationResult validationResult)
+        {
+            foreach (var error in validationResult.Errors)
+            {
+                Notificar(error.ErrorMessage);
+            }
+        }
+
+        protected void Notificar(string mensagem)
+        {
+            // Propagar esse erro até a camada de apresentação
+        }
+
+        protected bool ExecutarValidacao<TV, TE>(TV validacao, TE entidade) where TV : AbstractValidator<TE> where TE : Entity 
+        {
+            var validator = validacao.Validate(entidade);
+
+            if (validator.IsValid) return true;
+
+            Notificar(validator);
+
+            return false;
+        }
+    }
+}
